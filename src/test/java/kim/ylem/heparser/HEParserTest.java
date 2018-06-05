07f699077f5b83eb4fb5d3a143d2abdc86522b5c
ylemkimon <mail@ylem.kim>
2018-06-05 14:44:00 +0900

    Add unit tests

---
+++ src/test/java/kim/ylem/heparser/HEParserTest.java
@@ -0,0 +1,16 @@
+package kim.ylem.heparser;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HEParserTest {
+    @DisplayName("HEParser")
+    @ParameterizedTest(name = "\"{0}\" should parse to \"{1}\"")
+    @CsvFileSource(resources = "/parse-test.csv", numLinesToSkip = 1)
+    void toParse(String expr, String expected) {
+        assertEquals(expected, new HEParser(expr).parse());
+    }
+}
